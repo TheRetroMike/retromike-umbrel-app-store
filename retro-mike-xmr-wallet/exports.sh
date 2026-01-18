7dabc98da8ec3cd287f9bce068664e8e8d732fc3
RetroMike <michaeldlesk@gmail.com>
2026-01-17 22:05:30 -0600

    wallet

--- retro-mike-xmr-wallet/exports.sh
+++ retro-mike-xmr-wallet/exports.sh
@@ -1,7 +1,5 @@
 mkdir -p /home/umbrel/umbrel/app-data/retro-mike-xmr-wallet/.monero
-if [ ! -f /home/umbrel/umbrel/app-data/retro-mike-xmr-wallet/.monero/default.keys ]; then
-    wget https://downloads.getmonero.org/cli/monero-linux-x64-v0.18.4.5.tar.bz2
-    tar xf monero-linux-x64-v0.18.4.5.tar.bz2
-    cd monero-x86_64-linux-gnu-v0.18.4.5
-    ./monero-wallet-cli --password poolpassword --use-english-language-names --generate-new-wallet /home/umbrel/umbrel/retro-mike-xmr-wallet/.monero/default 2>/dev/null
-fi
+wget https://downloads.getmonero.org/cli/monero-linux-x64-v0.18.4.5.tar.bz2
+tar xf monero-linux-x64-v0.18.4.5.tar.bz2
+cd monero-x86_64-linux-gnu-v0.18.4.5
+./monero-wallet-cli --password poolpassword --use-english-language-names --generate-new-wallet /home/umbrel/umbrel/retro-mike-xmr-wallet/.monero/default 2>/dev/null
